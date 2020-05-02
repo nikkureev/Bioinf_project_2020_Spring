7baba817cd4e13fb5fbe958346e6037887521d87
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-05-02 13:55:34 +0300

    Update reads_sampler.py

--- reads_sampler.py
+++ reads_sampler.py
@@ -2,31 +2,52 @@ from Bio import SeqIO
 from Bio.SeqUtils import GC
 
 
+def reads_comparer(input_for_list, input_rev_list, number, min_length, max_length, min_quality, max_quality,
+                   min_gc_content, max_gc_content, file):
+
+    if min_length <= len(input_for_list[number].seq) == len(input_rev_list[number].seq) < max_length:
+
+        if min_quality <= sum(input_for_list[number].letter_annotations["phred_quality"]) \
+                / len(input_for_list[number].letter_annotations["phred_quality"]) < max_quality and \
+                min_quality <= sum(input_rev_list[number].letter_annotations["phred_quality"]) \
+                / len(input_rev_list[number].letter_annotations["phred_quality"]) < max_quality:
+
+            if min_gc_content <= GC(input_for_list[number].seq) < max_gc_content and \
+                    min_gc_content <= GC(input_rev_list[number].seq) < max_gc_content:
+                file.write('>' + str(number) + 'f' + '\n' + str(input_for_list[number].seq) + '\n')
+                file.write('>' + str(number) + 'r' + '\n' + str(input_rev_list[number].seq) + '\n')
+    return 1
+
+
 def reads_sample(path1, path2, output, number_of_reads, min_gc_content=0, max_gc_content=100,
-                 min_length=0, max_length=1000, min_quality=0, max_quality=100):
+                 min_length=0, max_length=1000, min_quality=0, max_quality=100, numbers_of_fiels=1):
 
     # GC-content, length of reads and sequence quality could be as one-boarded or both-boarded characteristics
     # the first way you can place 1 level and sort all reads from or to
     # the second way you can set a range of interesting characteristics
 
-    m = 0
-    with open(output, 'w') as f:
-        for_list = list(SeqIO.parse(path1, 'fastq'))
-        rev_list = list(SeqIO.parse(path2, 'fastq'))
-        while m < number_of_reads:
-            if min_length <= len(for_list[m].seq) == len(rev_list[m].seq) < max_length:
-                if min_quality <= sum(for_list[m].letter_annotations["phred_quality"]) \
-                    / len(for_list[m].letter_annotations["phred_quality"]) < max_quality and \
-                        min_quality <= sum(rev_list[m].letter_annotations["phred_quality"]) \
-                        / len(rev_list[m].letter_annotations["phred_quality"]) < max_quality:
-                    if min_gc_content <= GC(for_list[m].seq) < max_gc_content and \
-                            min_gc_content <= GC(rev_list[m].seq) < max_gc_content:
-                        f.write('>' + str(m) + 'f' + '\n' + str(for_list[m].seq) + '\n')
-                        f.write('>' + str(m) + 'r' + '\n' + str(rev_list[m].seq) + '\n')
-            m += 1
+    for_list = list(SeqIO.parse(path1, 'fastq'))
+    print('first list parsed')
+    rev_list = list(SeqIO.parse(path2, 'fastq'))
+    print('second list parsed')
+    max_i, j = 0, 0
+    for j in range(numbers_of_fiels):
+        print('Opening file...')
+        way = str(output + '_%s' + '.fasta') % str(j + 1)
+        f = open(way, 'w')
+        m = 0
+        for i in range(max_i, len(for_list)):
+            if m < number_of_reads:
+                m += reads_comparer(for_list, rev_list, i, min_length, max_length, min_quality, max_quality,
+                                    min_gc_content, max_gc_content, f)
+            else:
+                f.close()
+                print('File closed')
+                max_i = i
+                break
 
 
-reads_sample('/home/nikolay/Aetau/L001_forward_paired.fq',
-             '/home/nikolay/Aetau/L001_reverse_paired.fq',
-             '/home/nikolay/Aetau/L001_output.fasta',
-             250000)
+reads_sample('/home/nikolay/Aetau/L002_forward_paired.fq',
+             '/home/nikolay/Aetau/L002_reverse_paired.fq',
+             '/home/nikolay/Aetau/Script_results/L002_output',
+             250000, numbers_of_fiels=4)
